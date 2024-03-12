21aad6d6781c873f9bc29d58c56641b03b71e385
Juan23X <159858764+Juan23X@users.noreply.github.com>
2024-03-12 14:55:05 -0500

    Update tarea.js

--- tarea.js
+++ tarea.js
@@ -1,31 +1,45 @@
-const suma = (a,b) =>{
-  /**
+ /**
    * a y b son los parámetros de entrada.
    * La función debe devolver la suma de los parámetros de entrada
    * Debes generar el código para que la función haga lo solicitado.
    */
 
+const suma = (a,b) =>{
+ 
+
   return a + b;
 }
 
-const sonIguales = (x, y)=>{
-  // Devuelve "true" si "x" e "y" son iguales
+
+
+
+
+
+// Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Debes generar el código para que la función haga lo solicitado.
+
+const sonIguales = (x, y)=>{
+  
 
   return x === y;
 }
 
-function tienenMismaLongitud(str1, str2) {
-  // Devuelve "true" si las dos strings tienen la misma longitud
+
+
+// Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+
+function tienenMismaLongitud(str1, str2) {
+  
 
   return str1.length === str2.length;
 }
 
-function esPositivo(numero) {
-  /**
+
+
+/**
    * la función recibe como parámetro de entrada un número entero.
    * Devuelve como resultado una cadena de texto que indica si el 
    * número es positivo o negativo. 
@@ -34,6 +48,9 @@ function esPositivo(numero) {
    * Si el número es 0, devuelve false
    * Tu código:
    */
+
+function esPositivo(numero) {
+  
 
   if (numero > 0) {
     return "El número ingresado es positivo";
@@ -46,14 +63,18 @@ function esPositivo(numero) {
   }
 }
 
-const colors = (color)=>{
-  //La función recibe un color. Devolver el string correspondiente:
+
+
+//La función recibe un color. Devolver el string correspondiente:
   //En caso que el color recibido sea "blue", devuelve --> "This is blue"
   //En caso que el color recibido sea "red", devuelve --> "This is red"
   //En caso que el color recibido sea "green", devuelve --> "This is green"
   //En caso que el color recibido sea "orange", devuelve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+
+const colors = (color)=>{
+  
 
   const Colores = {
     blue: "This is blue",
@@ -67,11 +88,18 @@ const colors = (color)=>{
   return Colores[color] || Colordefault;
 }
 
-const fizzBuzz = (numero)  => {
-  // Si "numero" es divisible entre 3, devuelve "fizz"
+
+
+
+
+
+// Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+
+const fizzBuzz = (numero)  => {
+  
 
   if (numero % 3 === 0 && numero % 5 === 0) {
     return "fizzbuzz";
@@ -87,33 +115,54 @@ const fizzBuzz = (numero)  => {
   }
 }
 
-const devolverPrimerElemento = (array) => {
-  /**
+
+
+
+
+/**
    * La función recibe como parámetro de entrada un arreglo.
    * La función debe regresar el primer elemento del arreglo
    */
   // Tu código:
 
+const devolverPrimerElemento = (array) => {
+  
+
   return array[0];
 }
 
-const agregarItemAlFinalDelArray = (array, elemento) =>{
-  /**
+
+
+
+
+
+
+/**
    * La función recibe dos parámetros de entrada, un arreglo y un elemento
    * Se debe añadir el elemento al final del arreglo y luego regresar el arreglo
    */
   // Tu código:
 
+const agregarItemAlFinalDelArray = (array, elemento) =>{
+  
+
   array.push(elemento);
   return array;
 }
 
-const nuevoUsuario = (nombre, email, password) => {
-  /**
+
+
+
+
+
+/**
    * Crea un nuevo objeto con las propiedades coincidiendo con los argumentos
    * que se pasan a la función.  Devuelve el objeto
    */
   // Tu código:
+
+const nuevoUsuario = (nombre, email, password) => {
+  
 
   const Usuarios = {
 
@@ -126,11 +175,19 @@ const nuevoUsuario = (nombre, email, password) => {
   return Usuarios;
 }
 
-const verificarPassword = (usuario, password) => {
-  // Comprueba si el "password" enviado coincide con la propiedad "password" del objeto "usuario"
+
+
+
+
+
+
+// Comprueba si el "password" enviado coincide con la propiedad "password" del objeto "usuario"
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+
+const verificarPassword = (usuario, password) => {
+  
 
   if (usuario.password === password) {
     return true;
@@ -140,34 +197,54 @@ const verificarPassword = (usuario, password) => {
   }
 }
 
-const actualizarPassword =(usuario, nuevaPassword) => {
-  // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
+
+
+
+
+
+
+// Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
   // Devuelve el objeto
   // Tu código:
+
+const actualizarPassword =(usuario, nuevaPassword) => {
+  
 
   usuario.password = nuevaPassword;
   return usuario;
 
 }
 
-const agregarAmigo = (usuario, nuevoAmigo) => {
-  // "usuario" tiene una propiedad llamada "amigos" que es un array
+
+
+
+
+
+
+// "usuario" tiene una propiedad llamada "amigos" que es un array
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+
+const agregarAmigo = (usuario, nuevoAmigo) => {
 
   usuario.amigo.push(nuevoAmigo);
   return usuario;
 
 }
 
-const pasarUsuarioAPremium = usuarios => {
-  // "usuarios" es un array de objetos "usuario"
+
+
+
+// "usuarios" es un array de objetos "usuario"
   // Cada objeto "usuario" tiene la propiedad "esPremium"
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
 
+
+const pasarUsuarioAPremium = usuarios => {
+  
   usuarios.forEach((usuario) => {
   usuario.esPremium = true;
   })
@@ -175,13 +252,22 @@ const pasarUsuarioAPremium = usuarios => {
   return usuarios;
 }
 
-const sumarLikesDeUsuario = usuario => {
-  // "usuario" tiene una propiedad llamada "posts" que es un array
+
+
+
+
+
+
+
+// "usuario" tiene una propiedad llamada "posts" que es un array
   // "posts" es un array de objetos "post"
   // Cada objeto "post" tiene una propiedad llamada "likes" que es un entero (int/integer)
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+
+const sumarLikesDeUsuario = usuario => {
+  
 
   let suma = 0;
   usuario.posts.forEach((post) => (suma += post.likes));
@@ -190,7 +276,9 @@ const sumarLikesDeUsuario = usuario => {
 
 
 
-class Persona {
+
+
+
 // ---------------------------------------------------------------------------//
 //Crea el constructor de la clase "Persona"
 //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
@@ -203,6 +291,9 @@ class Persona {
   //   Edad: 22,
   //   Domicilio: 'Saavedra 123'
   //  }
+
+class Persona {
+
   constructor(nombre, apellido, edad, domicilio) {
 
     // Crea el constructor:
@@ -225,19 +316,31 @@ class Persona {
   }
 }
 
-const crearInstanciaPersona = (nombre, apellido, edad, dir)=>{
-  //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
+
+
+
+
+
+//Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+const crearInstanciaPersona = (nombre, apellido, edad, dir)=>{
+  
 
   const persona = new Persona(nombre, apellido, edad, dir);
   return persona;
 }
 
-const sumarArray = (numeros, cb) =>{
-  // Suma todos los números enteros (int/integers) de un array ("numeros")
+
+
+
+
+
+// Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+const sumarArray = (numeros, cb) =>{
+  
 
   let suma = numeros.reduce((num1, num2) => {
    return num1 + num2;
@@ -245,26 +348,48 @@ const sumarArray = (numeros, cb) =>{
   cb(suma);
 }
 
-const copiarEach = (array, cb) =>{
-  // Itera sobre la matriz "array" y pasa los valores a cb uno por uno
+
+
+
+
+
+
+
+
+// Itera sobre la matriz "array" y pasa los valores a cb uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en el array)
   // no tienes que devolver nada
+const copiarEach = (array, cb) =>{
+  
   
   array.forEach((value) => cb(value));
 }
 
-const operacionMatematica =(n1, n2, cb) =>{
-  //Vamos a recibir una función (cb) que realiza una operación matemática junto con dos números.
+
+
+
+
+
+//Vamos a recibir una función (cb) que realiza una operación matemática junto con dos números.
   //Devolver la función pasándole como argumentos los números recibidos.
   //Tu código:
 
+const operacionMatematica =(n1, n2, cb) =>{
+  
   return cb(n1, n2);
 }
 
-function filter(array) {
-  //Filtrar todos los elementos del array que comiencen con la letra "a".
+
+
+
+
+
+
+//Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+function filter(array) {
+  
 
   return array.filter((element) => {
     if (/[aá]/gi.test(element[0])) {
@@ -272,6 +397,11 @@ function filter(array) {
     }
   });
 } 
+
+
+
+
+
 
 
 /**
